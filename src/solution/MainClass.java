e68215c0ae32334485b10540bf053cf0862c5e1e
Basil-2020 <vtsyma@gmail.com>
2020-05-23 20:25:24 +0300

    add all files

---
+++ src/solution/MainClass.java
@@ -0,0 +1,8 @@
+package solution;
+
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("hsaaa");// TODO iewysagfiuwegf
+        // jkwehfvbilywe
+    }
+}
